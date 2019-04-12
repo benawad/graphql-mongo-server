cabc093ff21c16842d97c936fbbd951abf79c94d
Ben Awad <benawad97@gmail.com>
2019-04-11 19:02:15 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import express from "express";
+import mongoose from "mongoose";
+import { resolvers } from "./resolvers";
+import { typeDefs } from "./typeDefs";
+
+const startServer = async () => {
+  const app = express();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  server.applyMiddleware({ app });
+
+  await mongoose.connect("mongodb://localhost:27017/test3", {
+    useNewUrlParser: true
+  });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+};
+
+startServer();
